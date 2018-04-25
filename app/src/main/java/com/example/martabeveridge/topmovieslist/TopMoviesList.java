254dade0f5859660abd9cc9d729c349c1c0e63d9
martabeveridge <marta@beveridge.uk>
2018-04-25 13:24:50 +0100

    Project created. Edited layout design to include list view.

---
+++ app/src/main/java/com/example/martabeveridge/topmovieslist/TopMoviesList.java
@@ -0,0 +1,13 @@
+package com.example.martabeveridge.topmovieslist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TopMoviesList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top_movies_list);
+    }
+}
